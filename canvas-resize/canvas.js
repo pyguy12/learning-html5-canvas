e44d28835ba9bed5c14ef0c05976d533229b42c8
pyguy12 <wuwarren7360@gmail.com>
2019-08-31 21:11:11 -0500

    Added documentation on how to use arcs to draw circles

--- canvas-resize/canvas.js
+++ canvas-resize/canvas.js
@@ -31,3 +31,31 @@ c.strokeStyle = 'blue';
 
 //c.stroke() makes the line visible.
 c.stroke();
+
+//Arc / circle
+
+//c.beginPath() tells JS that you're making a separate path and it shouldn't connect the last one.
+c.beginPath();
+
+//c.arc(x,y,radius,starting degree, ending degree, direction)
+c.arc(500, 500, 30, 0, Math.PI * 2, false);
+c.strokeStyle = 'green';
+c.stroke();
+
+//Creating multiple circles
+
+for (let i = 0; i < 50; i++) {
+  c.beginPath();
+
+  //Math.random() returns a random value between 1 and 0.
+  c.arc(
+    Math.random() * window.innerWidth,
+    Math.random() * window.innerHeight,
+    30,
+    0,
+    Math.PI * 2,
+    false
+  );
+  c.strokeStyle = '#' + (((1 << 24) * Math.random()) | 0).toString(16);
+  c.stroke();
+}
